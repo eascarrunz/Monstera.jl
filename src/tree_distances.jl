bac864212e4055d2e3dfcfd8bb498dbfa360d8fd
eascarrunz <eascarrunz.dev@mailfence.com>
2024-04-01 19:20:56 -0300

    Disable progress bar for compatibility with Pluto

--- src/tree_distances.jl
+++ src/tree_distances.jl
@@ -1,21 +1,23 @@
-function rf_dist(trees)
+function rf_dist(trees; progress::Bool=true)
     ntree = length(trees)
     D = zeros(Int, ntree, ntree)
     bptable = bipartition_table(trees)
     Dsize = ntree * (ntree - 1) ÷ 2 
-    progmeter = Progress(
-        Dsize;
-        output = stderr,
-        dt=2,
-        desc="Computing pairwise distances",
-        showspeed=true,
-        enabled=! is_logging(stderr)
-        )
+    if progress
+        progmeter = Progress(
+            Dsize;
+            output = stderr,
+            dt=2,
+            desc="Computing pairwise distances",
+            showspeed=true,
+            enabled=! is_logging(stderr)
+            )
+    end
     for j in 1:ntree, i in 1:j
         @inbounds D[i, j] = special_countdiff(bptable.records[:, i], bptable.records[:, j])
-        next!(progmeter)
+        progress && next!(progmeter)
     end
-    finish!(progmeter)
+    progress && finish!(progmeter)
 
     return D
 end
@@ -44,6 +46,8 @@ that the trees are unrooted, and that polytomies are hard.
 
 - `diffonly=true`: if `true`, only bipartitions that are not shared between the two trees 
   will be considered. Only applicable to the `:ci` distance.
+- `progress=true`: whether to print a progress bar and ETA for long runs. Disable for Pluto
+compatibility.
 """ 
 function distance(trees, mode = :ci; kw...)
     if mode == :ci
